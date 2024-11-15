e9c6e9089e59a9b3513cddb38739849578a51a64
Mykola Yuryshynets <kolan4ick@gmail.com>
2024-11-15 20:36:15 +0200

    Implement locale detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   protected
 
@@ -10,5 +11,24 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:account_update,
                                       keys: [:name, :feasibility_threshold, :adjustment_delta,
                                              { feasibility_levels_attributes: %i[id title value linguistic] }])
+  end
+
+  def set_locale
+    locale = extract_locale_from_accept_language_header || I18n.default_locale
+    I18n.locale = locale
+  end
+
+  def extract_locale_from_accept_language_header
+    accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+
+    return if accept_language.blank?
+
+    locale = accept_language.scan(/^[a-z]{2}/).first
+
+    # Fallback to default locale if the extracted locale is not supported
+    I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
+  rescue StandardError => e
+    logger.error "Error extracting locale: #{e.message}"
+    nil
   end
 end
